dbb93b97416f7ad78927b1e1d7e17e6f1ea4087e
Muhammad Usama <muhammadusama30289@gmail.com>
2023-08-14 05:37:36 +0500

    Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login } from "./Route.js";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
