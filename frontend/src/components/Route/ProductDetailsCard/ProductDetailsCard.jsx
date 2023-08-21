1eef56403f40fb3724e07aafb87a8fc987973a5d
Muhammad Usama <muhammadusama30289@gmail.com>
2023-08-22 01:38:19 +0500

    ProductDetailsCard

--- frontend/src/components/Route/ProductDetailsCard/ProductDetailsCard.jsx
+++ frontend/src/components/Route/ProductDetailsCard/ProductDetailsCard.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { RxCross1 } from "react-icons/rx";
+import styles from "../../../styles/styles";
+import { AiOutlineMessage } from "react-icons/ai";
 
 const ProductDetailsCard = ({ setOpen, data }) => {
   const [count, setCount] = useState(1);
   const [click, setClick] = useState(false);
   const [select, useSelect] = useState(false);
+
+  const handleMessageSubmit = () => {};
 
   return (
     <div className="bg-white ">
@@ -16,6 +20,36 @@ const ProductDetailsCard = ({ setOpen, data }) => {
               className="absolute right-3 top-3 z-50"
               onClick={() => setOpen(false)}
             />
+
+            <div className="block w-full 800px:flex">
+              <div className="w-full 800px:w-[50%]">
+                <img src={data.image_Url[0].url} alt="" />
+                <div className="flex">
+                  <img
+                    src={data.shop.shop_avatar.url}
+                    className="w-[50px] h-[50px] rounded-full mr-2"
+                    alt=""
+                  />
+                  <div>
+                    <h3 className={`${styles.shop_name}`}>{data.shop.name}</h3>
+                    <h5 className="pb-3 text-[15px]">
+                      ({data.shop.ratings}) Ratings
+                    </h5>
+                  </div>
+                </div>
+                <div
+                  className={`${styles.button} bg-[#000] mt-4 rounded-[4px] h-11`}
+                  onClick={handleMessageSubmit}
+                >
+                  <span className="text-[#fff] flex items-center">
+                    Send Message <AiOutlineMessage className="ml-1" />
+                  </span>
+                </div>
+                <h5 className="text-[16px] text-[red] mt-5">
+                  ({data.total_sell}) Sold out
+                </h5>
+              </div>
+            </div>
           </div>
         </div>
       ) : null}
