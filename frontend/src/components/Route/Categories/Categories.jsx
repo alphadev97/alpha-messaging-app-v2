5bf9c230c74a76dacb60dd654e7b3c1e2749e294
Muhammad Usama <muhammadusama30289@gmail.com>
2023-08-21 06:19:59 +0500

    Categories

--- frontend/src/components/Route/Categories/Categories.jsx
+++ frontend/src/components/Route/Categories/Categories.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "../../../styles/styles";
-import { brandingData } from "../../../static/data";
+import { brandingData, categoriesData } from "../../../static/data";
+import { useNavigate } from "react-router-dom";
 
 const Categories = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div className={`${styles.section} sm:blobk`}>
@@ -20,6 +22,34 @@ const Categories = () => {
                 </div>
               </div>
             ))}
+        </div>
+      </div>
+
+      <div
+        className={`${styles.section} bg-white p-6 rounded-lg mb-12`}
+        id="categories"
+      >
+        <div className="grid grid-cols-1 gap-[5px] md:grid-cols-2 md:gap-[10px] lg:grid-cols-4 lg:gap-[20px] xl:grid-cols-5 xl:gap-[30px] ">
+          {categoriesData &&
+            categoriesData.map((i) => {
+              const handleSubmit = (i) => {
+                navigate(`/products?category=${i.title}`);
+              };
+              return (
+                <div
+                  className="w-full h-[100px] flex items-center justify-between cursor-pointer overflow-hidden"
+                  key={i.id}
+                  onClick={() => handleSubmit(i)}
+                >
+                  <h5 className={`text-[18px] leading-[1.3]`}>{i.title}</h5>
+                  <img
+                    src={i.image_Url}
+                    className="w-[120px] object-cover"
+                    alt=""
+                  />
+                </div>
+              );
+            })}
         </div>
       </div>
     </>
