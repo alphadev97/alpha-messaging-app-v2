254d89c939912961d295520d89f93d0151f3deba
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-22 20:49:20 +0500

    Footer

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,6 +6,7 @@ import BestDeals from "../components/Route/BestDeals/BestDeals";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
 import Events from "../components/Route/Events/Events";
 import Sponsored from "../components/Route/Sponsored";
+import Footer from "../components/Layout/Footer";
 
 const HomePage = () => {
   return (
