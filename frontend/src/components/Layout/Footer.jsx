60dccff0e9ff86773f32479442152664ffce9bdf
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-22 21:27:58 +0500

    Footer

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -5,8 +5,22 @@ const Footer = () => {
     <div className="bg-[#000] text-white ">
       <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#342ac8] py-7">
         <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-          <span className="text-[#56d879]">Subecribe</span> us for get news
+          <span className="text-[#56d879]">Subecribe</span> us for get news{" "}
+          <br />
+          Events and Offers
         </h1>
+        <div>
+          <input
+            type="text"
+            required
+            placeholder="Enter your email..."
+            className="text-gray-800
+                sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
+          />
+          <button className="bg-[#56d879] hover:bg-teal-500 duration-300 px-5 py-2.5 rounded-md text-whie md:w-auto w-full">
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
