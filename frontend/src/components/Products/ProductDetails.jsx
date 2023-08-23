2746dd72e8d398b453c574a309cb0a3cdfd61e47
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-24 01:27:39 +0500

    ProductDetailsPage

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "../../styles/styles";
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 
 const ProductDetails = ({ data }) => {
   const [count, setCount] = useState(1);
   const [click, setClick] = useState(false);
   const [select, setSelect] = useState(0);
   const navigate = useNavigate();
-  console.log(data);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+  const handleMessageSubmit = () => {
+    navigate("/inbox?conversation=5054fgdfgdfgdfg5453fgdfg");
+  };
 
   return (
     <div className="bg-white ">
@@ -55,6 +73,57 @@ const ProductDetails = ({ data }) => {
                   <h4 className={`${styles.productDiscountPrice}`}>
                     {data.discount_price}$
                   </h4>
+                  <h3 className={`${styles.price}`}>
+                    {data.price ? data.price + "$" : null}
+                  </h3>
+                </div>
+
+                <div className="flex items-center mt-12 justify-between pr-3">
+                  <div>
+                    <button
+                      className="bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
+                      onClick={decrementCount}
+                    >
+                      -
+                    </button>
+                    <span className="bg-gray-200 text-gray-800 font-medium px-4 py-[11px]">
+                      {count}
+                    </span>
+                    <button
+                      className="bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
+                      onClick={incrementCount}
+                    >
+                      +
+                    </button>
+                  </div>
+
+                  <div>
+                    {click ? (
+                      <AiFillHeart
+                        size={30}
+                        className="cursor-pointer "
+                        onClick={() => setClick(!click)}
+                        color={click ? "red" : "#333"}
+                        title="Remove from wishlist"
+                      />
+                    ) : (
+                      <AiOutlineHeart
+                        size={30}
+                        className="cursor-pointer "
+                        onClick={() => setClick(!click)}
+                        color={click ? "red" : "#333"}
+                        title="Add to wishlist"
+                      />
+                    )}
+                  </div>
+                </div>
+
+                <div
+                  className={`${styles.button} mt-6 rounded h-11 flex items-center`}
+                >
+                  <span className="text-white flex items-center">
+                    Add to cart <AiOutlineShoppingCart className="ml-1" />
+                  </span>
                 </div>
               </div>
             </div>
