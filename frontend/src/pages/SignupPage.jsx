fa941fe1fb8ffb8d7e32bfa211d0c42feacca7d6
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-23 19:28:12 +0500

    Login SignUp Redirection

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SignUp from "../components/SignUp/SignUp.jsx";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      navigate("/");
+    }
+  }, []);
   return (
     <div>
       <SignUp />
