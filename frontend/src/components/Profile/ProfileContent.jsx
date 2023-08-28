60cd52c84dc0071e5bcd04d4f47211141d513b31
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-28 20:35:22 +0500

    AllRefundOrders

--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -130,11 +130,110 @@ const ProfileContent = ({ active }) => {
           <AllOrders />
         </div>
       )}
+
+      {/* Refund Page */}
+      {active === 3 && (
+        <div>
+          <AllRefundOrders />
+        </div>
+      )}
     </div>
   );
 };
 
 const AllOrders = () => {
+  const orders = [
+    {
+      _id: "gsdjkfgbjfjbzjf545Sdf564z",
+      orderItems: [
+        {
+          name: "Iphone 14 Pro Max",
+        },
+      ],
+      totalPrice: 120,
+      orderStatus: "Processing",
+    },
+  ];
+
+  const columns = [
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      //   cellClassName: (params) => {
+      //     // return params.getValue(params.id, "status") === "Delivered"
+      //     return params.rowData.status === "Delivered"
+      //       ? "greenColor"
+      //       : "redColor";
+      //   },
+      cellClassName: (params) => {
+        return params.value === "Delivered" ? "greenColor" : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/order/${params.id}`}>
+              <Button>
+                <AiOutlineArrowRight size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  orders &&
+    orders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.orderItems.length,
+        total: "US$ " + item.totalPrice,
+        status: item.orderStatus,
+      });
+    });
+
+  return (
+    <div className="pl-8 pt-1">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSizeOptions={[10]}
+        disableRowSelectionOnClick
+        autoHeight
+      />
+    </div>
+  );
+};
+
+const AllRefundOrders = () => {
   const orders = [
     {
       _id: "gsdjkfgbjfjbzjf545Sdf564z",
