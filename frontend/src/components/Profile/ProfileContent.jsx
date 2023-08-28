174d4671fb257f9786f206aedd4b96c7e8e53ece
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-28 20:41:53 +0500

    TrackOrder

--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -6,6 +6,7 @@ import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { DataGrid } from "@mui/x-data-grid";
 import { Button } from "@mui/material";
+import { MdOutlineTrackChanges } from "react-icons/md";
 
 const ProfileContent = ({ active }) => {
   const { user } = useSelector((state) => state.user);
@@ -135,6 +136,13 @@ const ProfileContent = ({ active }) => {
       {active === 3 && (
         <div>
           <AllRefundOrders />
+        </div>
+      )}
+
+      {/* Track Order Page */}
+      {active === 5 && (
+        <div>
+          <TrackOrder />
         </div>
       )}
     </div>
@@ -292,6 +300,98 @@ const AllRefundOrders = () => {
             <Link to={`/order/${params.id}`}>
               <Button>
                 <AiOutlineArrowRight size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  orders &&
+    orders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.orderItems.length,
+        total: "US$ " + item.totalPrice,
+        status: item.orderStatus,
+      });
+    });
+
+  return (
+    <div className="pl-8 pt-1">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSizeOptions={[10]}
+        disableRowSelectionOnClick
+        autoHeight
+      />
+    </div>
+  );
+};
+
+const TrackOrder = () => {
+  const orders = [
+    {
+      _id: "gsdjkfgbjfjbzjf545Sdf564z",
+      orderItems: [
+        {
+          name: "Iphone 14 Pro Max",
+        },
+      ],
+      totalPrice: 120,
+      orderStatus: "Processing",
+    },
+  ];
+
+  const columns = [
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      //   cellClassName: (params) => {
+      //     // return params.getValue(params.id, "status") === "Delivered"
+      //     return params.rowData.status === "Delivered"
+      //       ? "greenColor"
+      //       : "redColor";
+      //   },
+      cellClassName: (params) => {
+        return params.value === "Delivered" ? "greenColor" : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 130,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/order/${params.id}`}>
+              <Button>
+                <MdOutlineTrackChanges size={20} />
               </Button>
             </Link>
           </>
