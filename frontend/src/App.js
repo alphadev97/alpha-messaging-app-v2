0c17c55f4b3018b3b27ea890405e0a48d9ccba49
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-29 01:30:44 +0500

    ProtectedRoutes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -45,7 +45,14 @@ const App = () => {
             <Route path="/best-selling" element={<BestSellingPage />} />
             <Route path="/events" element={<EventsPage />} />
             <Route path="/faq" element={<FAQPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route
+              path="/checkout"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <CheckoutPage />
+                </ProtectedRoute>
+              }
+            />
             <Route
               path="/profile"
               element={
