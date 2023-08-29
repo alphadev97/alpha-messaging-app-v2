1466a38af8d1988bd8e48cabdc4d1041439fd50d
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-29 23:47:18 +0500

    Logo

--- frontend/src/pages/ShopCreate.jsx
+++ frontend/src/pages/ShopCreate.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ShopCreateComponent from "../components/Shop/ShopCreateComponent";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const ShopCreate = () => {
+  const { isSeller, seller } = useSelector((state) => state.seller);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSeller === true) {
+      navigate(`/shop/${seller._id}`);
+    }
+  }, []);
   return (
     <div>
       <ShopCreateComponent />
