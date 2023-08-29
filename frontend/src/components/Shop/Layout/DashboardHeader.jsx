65558fe44feeff5ae5252279cf16a19b451a4567
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-30 00:53:20 +0500

    DashboardHeader

--- frontend/src/components/Shop/Layout/DashboardHeader.jsx
+++ frontend/src/components/Shop/Layout/DashboardHeader.jsx
@@ -1,7 +1,68 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/images/logo.png";
+import { AiOutlineGift } from "react-icons/ai";
+import { MdOutlineLocalOffer } from "react-icons/md";
+import { FiPackage, FiShoppingBag } from "react-icons/fi";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { backend_url } from "../../../server";
 
 const DashboardHeader = () => {
-  return <div>DashboardHeader</div>;
+  const { seller } = useSelector((state) => state.seller);
+
+  return (
+    <div className="w-full h-[80px] bg-white shadow sticky top-0 left-0 z-30 flex items-center justify-between px-4">
+      <div>
+        <Link to={"/dashboard"}>
+          <img src={logo} alt="" className="h-[80px]" />
+        </Link>
+      </div>
+
+      <div className="flex items-center">
+        <div className="flex items-center mr-4">
+          <Link to={"/dashboard/coupons"}>
+            <AiOutlineGift
+              color="#555"
+              size={30}
+              className="mx-5 cursor-pointer"
+            />
+          </Link>
+          <Link to={"/dashboard/events"}>
+            <MdOutlineLocalOffer
+              color="#555"
+              size={30}
+              className="mx-5 cursor-pointer"
+            />
+          </Link>
+          <Link to={"/dashboard/products"}>
+            <FiShoppingBag
+              color="#555"
+              size={30}
+              className="mx-5 cursor-pointer"
+            />
+          </Link>
+          <Link to={"/dashboard/orders"}>
+            <FiPackage color="#555" size={30} className="mx-5 cursor-pointer" />
+          </Link>
+          <Link to={"/dashboard/messages"}>
+            <BiMessageSquareDetail
+              color="#555"
+              size={30}
+              className="mx-5 cursor-pointer"
+            />
+          </Link>
+          <Link to={`/shop/${seller._id}`}>
+            <img
+              src={`${backend_url}${seller.avatar}`}
+              alt=""
+              className="w-[50px] h-[50px] rounded-full object-cover"
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardHeader;
