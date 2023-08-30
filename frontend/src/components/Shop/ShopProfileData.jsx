b655793ad4b865282d7652d121fb2029976a4438
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-30 22:29:50 +0500

    ShopProfileData

--- frontend/src/components/Shop/ShopProfileData.jsx
+++ frontend/src/components/Shop/ShopProfileData.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const ShopProfileData = () => {
-  return <div>ShopProfileData</div>;
+const ShopProfileData = ({ isOwner }) => {
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <div className="flex items-center">
+          <h5 className="font-[600] text-[20px] text-red-500 cursor-pointer">
+            Shop Products
+          </h5>
+        </div>
+
+        <div className="flex items-center">
+          <h5 className="font-[600] text-[20px] text-red-500 cursor-pointer">
+            Running Events
+          </h5>
+        </div>
+
+        <div className="flex items-center">
+          <h5 className="font-[600] text-[20px] text-red-500 cursor-pointer">
+            Shop Reviews
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShopProfileData;
