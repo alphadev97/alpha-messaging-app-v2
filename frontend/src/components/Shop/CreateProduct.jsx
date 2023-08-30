17245c86a36a9aa239ed9d90461efdf3fef42f76
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-31 00:57:16 +0500

    ShopCreateProduct

--- frontend/src/components/Shop/CreateProduct.jsx
+++ frontend/src/components/Shop/CreateProduct.jsx
@@ -1,7 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { categoriesData } from "../../static/data";
 
 const CreateProduct = () => {
-  return <div>CreateProduct</div>;
+  const { seller } = useSelector((state) => state.seller);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [images, setImages] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [tags, setTags] = useState("");
+  const [originalPrice, setOriginalPrice] = useState();
+  const [discountPrice, setDiscountPrice] = useState();
+  const [stock, setStock] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="w-[90%] 800px:w-[50%] bg-white shadow h-[80vh] rounded-[4px] p-3 overflow-y-scroll">
+      <h5 className="text-[30px] font-Poppins text-center">Create Product</h5>
+      {/* Create Product Form */}
+      <form onSubmit={handleSubmit}>
+        <br />
+        <div>
+          <label className="pb-2">
+            Name <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm"
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter your product name"
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label className="pb-2">
+            Description <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="description"
+            value={description}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm"
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Enter your product description"
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label className="pb-2">
+            Category <span className="text-red-500">*</span>
+          </label>
+          <select
+            className="w-full mt-2 border h-[35px] rounded-[5px]"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="Choose a category">Choose a category</option>
+            {categoriesData &&
+              categoriesData.map((i) => (
+                <option value={i.title} key={i.title}>
+                  {i.title}
+                </option>
+              ))}
+          </select>
+        </div>
+
+        <br />
+
+        <div>
+          <label className="pb-2">Tags</label>
+          <input
+            type="text"
+            name="tags"
+            value={tags}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm"
+            onChange={(e) => setTags(e.target.value)}
+            placeholder="Enter your product tags"
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label className="pb-2">Original Price</label>
+          <input
+            type="number"
+            name="price"
+            value={originalPrice}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm"
+            onChange={(e) => setOriginalPrice(e.target.value)}
+            placeholder="Enter your product price"
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label className="pb-2">
+            Price (With Discount) <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="number"
+            name="price"
+            value={discountPrice}
+            className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm"
+            onChange={(e) => setDiscountPrice(e.target.value)}
+            placeholder="Enter your product price with discount"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateProduct;
