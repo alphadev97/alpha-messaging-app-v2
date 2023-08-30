bace54da3f7055ac956b3c8dc81cdc402b3e966c
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-30 23:43:22 +0500

    ShopProfileData

--- frontend/src/components/Shop/ShopProfileData.jsx
+++ frontend/src/components/Shop/ShopProfileData.jsx
@@ -1,41 +1,56 @@
 import React, { useState } from "react";
 import { productData } from "../../static/data";
 import ProductCard from "../Route/ProductCard/ProductCard";
+import { Link } from "react-router-dom";
+import styles from "../../styles/styles";
 
 const ShopProfileData = ({ isOwner }) => {
   const [active, setActive] = useState(1);
 
   return (
     <div className="w-full">
-      <div className="flex w-full items-center">
-        <div className="flex items-center" onClick={() => setActive(1)}>
-          <h5
-            className={`font-[600] text-[20px] ${
-              active === 1 ? "text-red-500" : "text-[#333]"
-            } cursor-pointer pr-[20px]`}
-          >
-            Shop Products
-          </h5>
-        </div>
+      <div className="flex w-full items-center justify-between">
+        <div className="w-full flex">
+          <div className="flex items-center" onClick={() => setActive(1)}>
+            <h5
+              className={`font-[600] text-[20px] ${
+                active === 1 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
+            >
+              Shop Products
+            </h5>
+          </div>
 
-        <div className="flex items-center" onClick={() => setActive(2)}>
-          <h5
-            className={`font-[600] text-[20px] ${
-              active === 2 ? "text-red-500" : "text-[#333]"
-            } cursor-pointer pr-[20px]`}
-          >
-            Running Events
-          </h5>
-        </div>
+          <div className="flex items-center" onClick={() => setActive(2)}>
+            <h5
+              className={`font-[600] text-[20px] ${
+                active === 2 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
+            >
+              Running Events
+            </h5>
+          </div>
 
-        <div className="flex items-center" onClick={() => setActive(3)}>
-          <h5
-            className={`font-[600] text-[20px] ${
-              active === 3 ? "text-red-500" : "text-[#333]"
-            } cursor-pointer pr-[20px]`}
-          >
-            Shop Reviews
-          </h5>
+          <div className="flex items-center" onClick={() => setActive(3)}>
+            <h5
+              className={`font-[600] text-[20px] ${
+                active === 3 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
+            >
+              Shop Reviews
+            </h5>
+          </div>
+        </div>
+        <div>
+          {isOwner && (
+            <div>
+              <Link to={"/dashboard"}>
+                <div className={`${styles.button} !rounded-[4px] h-[42px]`}>
+                  <span className="text-white">Go Dashboard</span>
+                </div>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
 
