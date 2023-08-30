6576afba1d6683795b17750b85c308fbab0f0031
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-30 22:36:34 +0500

    ShopProfileData

--- frontend/src/components/Shop/ShopProfileData.jsx
+++ frontend/src/components/Shop/ShopProfileData.jsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ShopProfileData = ({ isOwner }) => {
+  const [active, setActive] = useState(1);
+
   return (
     <div className="w-full">
-      <div className="flex w-full items-center justify-between">
+      <div className="flex w-full items-center">
         <div className="flex items-center">
-          <h5 className="font-[600] text-[20px] text-red-500 cursor-pointer">
+          <h5
+            className={`font-[600] text-[20px] text-red-500 cursor-pointer pr-[20px]`}
+          >
             Shop Products
           </h5>
         </div>
 
         <div className="flex items-center">
-          <h5 className="font-[600] text-[20px] text-red-500 cursor-pointer">
+          <h5 className="font-[600] text-[20px]  cursor-pointer pr-[20px]">
             Running Events
           </h5>
         </div>
 
         <div className="flex items-center">
-          <h5 className="font-[600] text-[20px] text-red-500 cursor-pointer">
+          <h5 className="font-[600] text-[20px]  cursor-pointer pr-[20px]">
             Shop Reviews
           </h5>
         </div>
