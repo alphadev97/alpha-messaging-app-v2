46660b435c3fcc0e47f3fed708edd489c31e3364
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-30 23:03:58 +0500

    ShopProfileData

--- frontend/src/components/Shop/ShopProfileData.jsx
+++ frontend/src/components/Shop/ShopProfileData.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { productData } from "../../static/data";
+import ProductCard from "../Route/ProductCard/ProductCard";
 
 const ShopProfileData = ({ isOwner }) => {
   const [active, setActive] = useState(1);
@@ -6,25 +8,44 @@ const ShopProfileData = ({ isOwner }) => {
   return (
     <div className="w-full">
       <div className="flex w-full items-center">
-        <div className="flex items-center">
+        <div className="flex items-center" onClick={() => setActive(1)}>
           <h5
-            className={`font-[600] text-[20px] text-red-500 cursor-pointer pr-[20px]`}
+            className={`font-[600] text-[20px] ${
+              active === 1 ? "text-red-500" : "text-[#333]"
+            } cursor-pointer pr-[20px]`}
           >
             Shop Products
           </h5>
         </div>
 
-        <div className="flex items-center">
-          <h5 className="font-[600] text-[20px]  cursor-pointer pr-[20px]">
+        <div className="flex items-center" onClick={() => setActive(2)}>
+          <h5
+            className={`font-[600] text-[20px] ${
+              active === 2 ? "text-red-500" : "text-[#333]"
+            } cursor-pointer pr-[20px]`}
+          >
             Running Events
           </h5>
         </div>
 
-        <div className="flex items-center">
-          <h5 className="font-[600] text-[20px]  cursor-pointer pr-[20px]">
+        <div className="flex items-center" onClick={() => setActive(3)}>
+          <h5
+            className={`font-[600] text-[20px] ${
+              active === 3 ? "text-red-500" : "text-[#333]"
+            } cursor-pointer pr-[20px]`}
+          >
             Shop Reviews
           </h5>
         </div>
+      </div>
+
+      <br />
+
+      <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
+        {productData &&
+          productData.map((i, index) => (
+            <ProductCard data={i} key={index} isShop={true} />
+          ))}
       </div>
     </div>
   );
