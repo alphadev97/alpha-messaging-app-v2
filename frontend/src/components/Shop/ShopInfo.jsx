005bc57cba2cdf9fe27565d04408617048c42de9
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-30 21:44:16 +0500

    ShopInfo

--- frontend/src/components/Shop/ShopInfo.jsx
+++ frontend/src/components/Shop/ShopInfo.jsx
@@ -8,8 +8,13 @@ const ShopInfo = ({ isOwner }) => {
   return (
     <div className="w-full py-5">
       <div className="w-full flex items-center justify-center">
-        <img src={`${backend_url}${seller?.avatar}`} alt="" />
+        <img
+          src={`${backend_url}${seller?.avatar}`}
+          alt=""
+          className="w-[150px] h-[150px] object-cover rounded-full"
+        />
       </div>
+      <h3 className="text-center py-2 text-[20px]">{seller.name}</h3>
     </div>
   );
 };
