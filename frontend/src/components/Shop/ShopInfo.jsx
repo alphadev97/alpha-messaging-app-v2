084f5ae3d06ac3ad0ab32ed5d503ba269b08cd3b
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-30 22:16:22 +0500

    ShopInfo

--- frontend/src/components/Shop/ShopInfo.jsx
+++ frontend/src/components/Shop/ShopInfo.jsx
@@ -6,15 +6,24 @@ const ShopInfo = ({ isOwner }) => {
   const { seller } = useSelector((state) => state.seller);
 
   return (
-    <div className="w-full py-5">
-      <div className="w-full flex items-center justify-center">
-        <img
-          src={`${backend_url}${seller?.avatar}`}
-          alt=""
-          className="w-[150px] h-[150px] object-cover rounded-full"
-        />
+    <div>
+      <div className="w-full py-5">
+        <div className="w-full flex items-center justify-center">
+          <img
+            src={`${backend_url}${seller?.avatar}`}
+            alt=""
+            className="w-[150px] h-[150px] object-cover rounded-full"
+          />
+        </div>
+        <h3 className="text-center py-2 text-[20px]">{seller.name}</h3>
+        <p className="text-[16px] text-[#000000a6] p-[10px] flex items-center">
+          {seller.description}
+        </p>
       </div>
-      <h3 className="text-center py-2 text-[20px]">{seller.name}</h3>
+      <div className="p-3">
+        <h5 className="font-[600]">Address</h5>
+        <h4 className="text-[#000000a6]">{seller.address}</h4>
+      </div>
     </div>
   );
 };
