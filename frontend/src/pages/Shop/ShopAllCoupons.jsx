f864992535e1caaf863591965a636eff56ff02fb
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-05 22:55:09 +0500

    CouponCode

--- frontend/src/pages/Shop/ShopAllCoupons.jsx
+++ frontend/src/pages/Shop/ShopAllCoupons.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
+import DashboardSideBar from "../../components/Shop/Layout/DashboardSideBar";
+import AllCouponCodes from "../../components/Shop/AllCouponCodes.jsx";
 
 const ShopAllCoupons = () => {
-  return <div>ShopAllCoupons</div>;
+  return (
+    <div>
+      <DashboardHeader />
+
+      <div className="flex justify-between w-full">
+        <div className="w-[80px] 800px:w-[330px]">
+          <DashboardSideBar active={9} />
+        </div>
+        <div className="w-full justify-center flex">
+          <AllCouponCodes />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShopAllCoupons;
