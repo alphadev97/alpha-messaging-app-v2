995fdfa833d2525e07707250adc034268a0fea56
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-05 22:28:24 +0500

    couponCodeController

---
+++ backend/controller/couponCodeController.js
@@ -0,0 +1,31 @@
+import express from "express";
+import catchAsyncErrors from "../middleware/catchAsyncErrors.js";
+import shopModel from "../model/shopModel.js";
+import ErrorHandler from "../utils/ErrorHandler.js";
+import { isSeller } from "../middleware/auth.js";
+import couponCodeModel from "../model/couponCodeModel.js";
+const router = express.Router();
+
+// create coupon code
+router.post(
+  "/create-coupon-code",
+  isSeller,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const isCouponCode = await couponCodeModel.find({ name: req.body.name });
+
+      if (isCouponCode) {
+        return next(new ErrorHandler("Coupon code already exist!", 400));
+      }
+
+      const CouponCode = await couponCodeModel.create(req.body);
+
+      res.status(201).json({
+        success: true,
+        CouponCode,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
