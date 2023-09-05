a14c578a7be66e9f6b10b9c8964ec08715fe9838
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-05 20:48:58 +0500

    productListing

--- frontend/src/components/Shop/CreateProduct.jsx
+++ frontend/src/components/Shop/CreateProduct.jsx
@@ -26,7 +26,7 @@ const CreateProduct = () => {
     }
     if (success) {
       toast.success("Product created successfully!");
-      navigate("/dashboard");
+      navigate("/dashboard-products");
       window.location.reload();
     }
   }, [dispatch, error, success]);
