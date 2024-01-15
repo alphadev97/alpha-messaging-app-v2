81c8a148909b38874501aab4e6bb5a6cfbc38aca
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-15 22:28:40 +0500

    admin dashboard with styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Loader from "./components/loader";
+import Header from "./components/header";
 
 const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
@@ -30,6 +31,7 @@ const App = () => {
   return (
     <Router>
       {/* Header */}
+      <Header />
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
