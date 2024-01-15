19b03e60d1ae0b7457bd418d1a04e609c801c95e
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-16 01:13:53 +0500

    Header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,5 +1,28 @@
+import { FaSearch, FaShoppingBag, FaSignInAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const user = { _id: "" };
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
+
+      {user?._id ? (
+        <></>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
+    </nav>
+  );
 };
 
 export default Header;
