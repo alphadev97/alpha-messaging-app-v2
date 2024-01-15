8171d86f1f064d853e7fae7b7ab27205971c4ed6
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-15 18:18:29 +0500

    Pages with lazy load and suspense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/loader";
+
+const Home = lazy(() => import("./pages/home"));
+const Search = lazy(() => import("./pages/search"));
+const Cart = lazy(() => import("./pages/cart"));
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      {/* Header */}
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/loader.tsx
@@ -0,0 +1,5 @@
+const Loader = () => {
+  return <div>Laoding</div>;
+};
+
+export default Loader;
