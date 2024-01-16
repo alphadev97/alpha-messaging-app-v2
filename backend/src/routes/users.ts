107a1dade424dce99ee908b715eaa12fe5e37039
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-16 22:00:51 +0500

    form validation
    form validation with express validator

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,40 +1,58 @@
 import express, { Request, Response } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
 
 const router = express.Router();
 
 // /api/users/resgiter
-router.post("/register", async (req: Request, res: Response) => {
-  try {
-    let user = await User.findOne({
-      email: req.body.email,
-    });
+router.post(
+  "/register",
+  [
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-    if (user) {
-      return res.status(400).json({ message: "User already exists!" });
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
     }
 
-    user = new User(req.body);
-    await user.save();
+    try {
+      let user = await User.findOne({
+        email: req.body.email,
+      });
 
-    const token = jwt.sign(
-      { userId: user.id },
-      process.env.JWT_SECRET_KEY as string,
-      { expiresIn: "1d" }
-    );
+      if (user) {
+        return res.status(400).json({ message: "User already exists!" });
+      }
 
-    res.cookie("auth_token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 86400000,
-    });
+      user = new User(req.body);
+      await user.save();
 
-    return res.status(200).send({ message: "User registered OK" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Something went wrong!" });
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        { expiresIn: "1d" }
+      );
+
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 86400000,
+      });
+
+      return res.status(200).send({ message: "User registered OK" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Something went wrong!" });
+    }
   }
-});
+);
 
 export default router;
