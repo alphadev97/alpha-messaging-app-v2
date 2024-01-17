346ff5da049649840f25546def794316563885c5
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-17 18:43:30 +0500

    Responsive container

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,10 @@ export default {
   theme: {
     extend: {},
     container: {
-      padding: "10rem",
+      padding: {
+        sm: "1rem",
+        md: "10rem",
+      },
     },
   },
   plugins: [],
