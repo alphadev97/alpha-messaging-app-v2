294f611af11fa783e7e139a43a02f4febd32f7b5
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-17 23:57:50 +0500

    set token to context

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,25 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("MongoDB is Connected!"));
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
   return (
     <div className="bg-blue-800 py-6 ">
       <div className="container mx-auto flex justify-between">
@@ -8,12 +10,20 @@ const Header = () => {
           <Link to={"/"}>AlphaBookingApp</Link>
         </span>
         <span className="flex space-x-2">
-          <Link
-            className="flex items-center bg-white text-blue-600 px-3 font-bold hover:bg-gray-100"
-            to={"/sign-in"}
-          >
-            Sign In
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <Link to={"/my-booking"}>My Bookings</Link>
+              <Link to={"/my-hotels"}>My Hotels</Link>
+              <button>Sign Out</button>
+            </>
+          ) : (
+            <Link
+              className="flex items-center bg-white text-blue-600 px-3 font-bold hover:bg-gray-100"
+              to={"/sign-in"}
+            >
+              Sign In
+            </Link>
+          )}
         </span>
       </div>
     </div>
