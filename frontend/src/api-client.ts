5335eba2cb6c359e77fd2edd3114c5b73b526c5f
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-18 01:20:43 +0500

    SignIn page with user signin

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,4 +1,5 @@
 import { RegisterFormData } from "./pages/Register";
+import { SignInFormData } from "./pages/SignIn";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -17,6 +18,25 @@ export const register = async (formData: RegisterFormData) => {
   if (!response.ok) {
     throw new Error(responseBody.message);
   }
+};
+
+export const signIn = async (formData: SignInFormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  const body = await response.json();
+
+  if (!response.ok) {
+    throw new Error(body.message);
+  }
+
+  return body;
 };
 
 export const validateToken = async () => {
