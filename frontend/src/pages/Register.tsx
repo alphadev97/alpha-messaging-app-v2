52ff6ebdfc2c50463e93e0ebe634793ad589b20d
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-17 18:30:04 +0500

    Register Page

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <form className="flex flex-col gap-5">
+      <h2 className="text-3xl font-bold">Create an Account</h2>
+    </form>
+  );
+};
+
+export default Register;
