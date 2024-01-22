55579780c4188e871856cdde72048d6fa9e9eaa6
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-22 23:48:05 +0500

    user password test

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -11,7 +11,7 @@ test("should allow the user to sign in", async ({ page }) => {
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
   await page.locator("[name=email]").fill("1@1.com");
-  await page.locator("[name=password]").fill("password");
+  await page.locator("[name=password]").fill("password123");
 
   await page.getByRole("button", { name: "Log In" }).click();
 
