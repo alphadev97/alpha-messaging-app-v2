4afa6f0e3ca1cdeec92015ee2d0fe77a21c56e9c
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-22 20:41:37 +0500

    first test for auth - sign in

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -1,18 +1,22 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const UI_URL = "http://localhost:5173";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test("should allow the user to sign in", async ({ page }) => {
+  await page.goto(UI_URL);
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  // get the sign in button
+  await page.getByRole("link", { name: "Sign In" }).click();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.locator("[name=email]").fill("test@test.com");
+  await page.locator("[name=password]").fill("password");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Sign in Successfull")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
