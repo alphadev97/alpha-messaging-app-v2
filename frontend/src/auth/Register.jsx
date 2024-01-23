ab1ccde41b56daa5c16198033acfb6d429a25c3f
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-23 21:54:06 +0500

    Register Form

--- frontend/src/auth/Register.jsx
+++ frontend/src/auth/Register.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-  return <div className="bg-blue-50 h-screen"></div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="bg-blue-50 h-screen flex items-center">
+      <form className="w-64 mx-auto">
+        <input
+          type="text"
+          placeholder="username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="block w-full rounded-sm p-2 mb-2 border"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
+        <button className="p-2 bg-blue-500 text-white w-full rounded-sm">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
