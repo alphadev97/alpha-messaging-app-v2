e97884bd5bece79c02e10f6dcae229d2e3f3798b
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-23 22:58:34 +0500

    mongoose connect

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 const port = 5000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.get("/test", (req, res) => {
   res.json("test ok");
