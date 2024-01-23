9f171c9e5fb5aa578b7b411cff5f00c3e4525bec
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-23 22:13:47 +0500

    backend init

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.json("test ok");
+});
+
+app.listen(port, () => {
+  console.log(`Backend is running on port ${port}`);
+});
