44d4ef27dc7f630aa5248d19e137913f9114d824
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-23 23:53:39 +0500

    authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,20 @@
 import { UserModel } from "../models/User.js";
+import { jwt } from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, password } = req.body;
 
   const newUser = await UserModel.create({ username, password });
+
+  jwt.sign(
+    { userId: newUser._id },
+    process.env.JWT_SECRET_KEY,
+    (err, token) => {
+      if (err) throw err;
+
+      res.cookie("token", token).status(201).json("ok");
+    }
+  );
 
   try {
     await newUser.save();
