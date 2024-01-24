44954b9c27d08ebb308d868eedb06e0cb001d32c
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-24 20:01:05 +0500

    App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useContext } from "react";
-import { UserContext } from "./context/UserContext";
 import Register from "./auth/Register";
 import axios from "axios";
 import SignIn from "./auth/SignIn";
@@ -10,12 +8,6 @@ import Home from "./pages/Home";
 function App() {
   axios.defaults.baseURL = "http://localhost:5000";
   axios.defaults.withCredentials = true;
-
-  const { username } = useContext(UserContext);
-
-  if (username) {
-    return "Logged In " + username;
-  }
 
   return (
     <Router>
