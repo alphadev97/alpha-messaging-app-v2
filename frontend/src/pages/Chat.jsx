b93f711b0dfc3904a6ff0df721be054f99acc5e7
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-25 22:47:34 +0500

    Chat component

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,8 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import Avatar from "../components/Avatar";
+import Logo from "../components/Logo";
+import { UserContext } from "../context/UserContext";
 
 const Chat = () => {
   const [ws, setWs] = useState(null);
   const [onlinePeople, setOnlinePeople] = useState({});
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const { username, id } = useContext(UserContext);
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:5000");
@@ -29,15 +34,55 @@ const Chat = () => {
     }
   };
 
+  const onlinePeopleExclSelf = { ...onlinePeople };
+  delete onlinePeopleExclSelf[id];
+
   return (
     <div className="flex h-screen">
-      <div className="bg-white w-1/3 p-2">
-        {Object.keys(onlinePeople).map((userId) => (
-          <div key={userId}>{onlinePeople[userId]}</div>
+      <div className="bg-white w-1/3">
+        <Logo />
+        {Object.keys(onlinePeopleExclSelf).map((userId) => (
+          <div
+            key={userId}
+            onClick={() => setSelectedUserId(userId)}
+            className={`${
+              userId === selectedUserId ? "bg-blue-100" : ""
+            } border-b border-gray-100 flex gap-2 cursor-pointer`}
+          >
+            {userId === selectedUserId && (
+              <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+            )}
+            <div className="flex gap-2 py-2 pl-4 items-center">
+              <Avatar username={onlinePeople[userId]} userId={userId} />
+              <span>{onlinePeople[userId]}</span>
+            </div>
+          </div>
         ))}
       </div>
       <div className="flex flex-col bg-blue-100 w-2/3 p-2">
-        <div className="flex-grow">Message</div>
+        <div className="flex-grow">
+          {!selectedUserId && (
+            <div className="h-full flex items-center justify-center font-bold">
+              <div className="text-gray-400 flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+                  />
+                </svg>
+                Select a person to chat
+              </div>
+            </div>
+          )}
+        </div>
         <div className="flex gap-2">
           <input
             type="text"
