dae6c741a5cd6b85c60877533ee2850e713e1d7d
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-29 23:55:58 +0500

    Offline indicator

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -22,6 +22,9 @@ const Avatar = ({ userId, username, online }) => {
       {online && (
         <div className="absolute w-3 h-3 bg-green-500 bottom-0 right-0 rounded-full border border-white"></div>
       )}
+      {!online && (
+        <div className="absolute w-3 h-3 bg-gray-400 bottom-0 right-0 rounded-full border border-white"></div>
+      )}
     </div>
   );
 };
