7ee6eac8ad7bbe9b031c4ad3376419c92bde2f1a
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-02-06 00:31:17 +0500

    UserModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -20,6 +20,9 @@ const UserSchema = new mongoose.Schema({
     default:
       "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
   },
+  bio: {
+    type: String,
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
