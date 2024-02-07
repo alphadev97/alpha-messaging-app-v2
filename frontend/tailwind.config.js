ecb4274caf072a064269479930e252130874fdd9
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-02-07 19:52:41 +0500

    Themes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgba(var(--background))",
+        text: "rgba(var(--text))",
+        button: "rgba(var(--button))",
+        buttonText: "rgba(var(--button-text))",
+      },
+    },
   },
   plugins: [],
 };
