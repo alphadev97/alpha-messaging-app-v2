36ec4b2d77563504a13e8efbd2d7626c96824f37
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-02-07 20:14:09 +0500

    Login Dark Light

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,48 +1,54 @@
 import Logo from "../../components/Logo";
 import { Link } from "react-router-dom";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
 import { useForm } from "react-hook-form";
+import ThemeToggle from "../../lib/ThemeToggle";
+
+// ANTD
+
+import { MailOutlined, KeyOutlined } from "@ant-design/icons";
+import { Input } from "antd";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="flex flex-col h-[100vh] w-full items-center">
+    <div className="bg-background flex flex-col h-[100vh] w-full items-center">
       <div className="flex flex-col w-full h-full items-center justify-center gap-4">
+        <ThemeToggle />
         <Logo />
 
         <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
-          <TextField
-            id="email"
-            label="Enter Your Email"
-            variant="outlined"
-            className="w-[400px]"
+          <Input
+            size="large"
+            placeholder="Enter Your Email"
+            className="w-[400px] bg-[#F4EAE0] border-text flex items-center p-3"
+            prefix={
+              <MailOutlined className="bg-black p-2 text-[#F4EAE0] rounded-full mr-2" />
+            }
             {...register("email")}
           />
 
-          <TextField
-            id="password"
-            label="Enter Your Password"
-            variant="outlined"
-            className="w-[400px]"
+          <Input
+            size="large"
+            placeholder="Enter Your Password"
+            className="w-[400px] bg-[#F4EAE0] border-text flex items-center p-3"
+            prefix={
+              <KeyOutlined className="bg-black p-2 text-[#F4EAE0] rounded-full mr-2" />
+            }
             {...register("password")}
           />
 
-          <Button
-            type="submit"
-            variant="contained"
-            sx={{
-              padding: "0.75rem",
-              width: "400px",
-            }}
+          <button
+            type="submite"
+            className="bg-button text-buttonText w-full p-3 rounded-md "
           >
-            Sign in
-          </Button>
+            Sign In
+          </button>
         </form>
 
-        <p>
+        <p className="text-text ">
           Don't Have Account?{" "}
           <Link to="/signup" className="underline font-semibold">
             Create your account
