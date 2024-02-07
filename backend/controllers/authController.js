aa104a0458ae2d3ed29f8747dc46639a81de76a9
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-02-08 01:46:37 +0500

    authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,19 @@
-export const signup = (req, res, next) => {};
+import bcryptjs from "bcryptjs";
+import User from "../models/userModel.js";
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.status(201).json({ success: true, message: "User is created" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(401)
+      .json({ success: false, message: "Problem in user creation" });
+  }
+};
